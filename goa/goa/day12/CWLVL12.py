cadbfe399be7af8517926cb637c2338cab4e5a11
Giorgi965 <giogerliani229@gmail.com>
2025-05-28 18:34:19 +0400

    Delete goa directory

--- goa/goa/day12/CWLVL12.py
+++
@@ -1,5 +0,0 @@
-print(False and True or True and False or True) #true
-print(False and True or True and False or True) #true
-print((True and False) or (False or True and True)) #true
-print(True and True or False and False or True) #true
-print((False or True) and (True or False and True)) #true
