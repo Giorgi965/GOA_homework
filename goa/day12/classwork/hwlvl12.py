5e1051c4c8166672fb029238d3e9b1c695d90a49
Giorgi965 <giogerliani229@gmail.com>
2025-06-15 18:12:06 +0400

    Add files via upload

---
+++ goa/day12/classwork/hwlvl12.py
@@ -0,0 +1,10 @@
+print(True or False)
+print(True and False)
+print(True and True)
+print(False or False)
+
+
+print(True and False)
+print(True and True)
+print(False and True)
+print(True and True)
