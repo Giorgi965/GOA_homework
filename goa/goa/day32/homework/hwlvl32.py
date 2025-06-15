e060be10c7d5992c5651cdcaadbcf2810380a7ac
Giorgi965 <giogerliani229@gmail.com>
2025-06-15 18:08:56 +0400

    Add files via upload

---
+++ goa/goa/day32/homework/hwlvl32.py
@@ -0,0 +1,20 @@
+inp=str(input('enter string'))
+
+print(inp.title())
+
+sia='Banana','apple','pear','mango','cherry','rose'
+print(sia[0].lower())
+print(sia[1].upper())
+print(sia[2].capitalize())
+print(sia[3].count('o'))
+print(sia[4].replace("r",'d'))
+print(sia[5].title())
+
+
+#lower აპატარავებს ასოებს სტრინგში 
+#upper ადიდებს ასოებს სტრინგში
+#capitalize ადიდებს ყველა ასოსო სტრინგში
+#count ითვლის იმ ასოების რაოდენობას სტრინგში რომელსაც მივუთითებთ
+#replace შეუძლია შეცვალოს კონკრეტული ასო იმ ასოთ რასაც ჩვენ მივუთითებთ
+#title ადიდებს პირველ ასოს სიტყვაში  არააქ მნიშვნელობა რითი დავყობთ
+#string  არის immutsable იმიტომ რომ მის მნიშვნელობას ვერ შეცვლი 
---
+++ goa/goa/day4/classwork/cwlvl4.py
@@ -0,0 +1,17 @@
+from turtle import
+country="argentina 
+#აკლია ცალი ბრჭყალი" \
+
+item-"apple"
+#ტოლობის მაგივრად წერია გამოკლება
+
+
+print("hello world"
+      #აკლია ბრჩხილი
+
+print("hello world)
+      #აკლია ბრჭყალი"
+
+
+print("hello world"}
+#შეცვლილია ბრჩხილი
