05d97ddac568e29e8815f439d34f19a099adc073
Giorgi965 <giogerliani229@gmail.com>
2025-06-27 00:45:23 +0400

    Add files via upload

---
+++ goa/day18/classwork/cwlvl18.py
@@ -0,0 +1,22 @@
+
+for i in range(1, 11, 2): 
+    print(i)
+
+
+
+for i in range(1, 101):  
+    print(f"{i} GOA Best!") 
+
+
+
+numbers = input("შეიყვანეთ 10 რიცხვი, გააწყვეთ ისინი ცარიელი სივრცით: ")
+
+
+
+
+for i in range(10):
+    num = int(input("შეიყვანეთ რიცხვი"))
+
+
+for number in numbers:
+    print(number)
---
+++ goa/day8/classwork/cwlvl8.py
@@ -0,0 +1,41 @@
+#type() ფუნქცია გამოიყენება იმისათვის, რომ გავიგოთ რომელ ტიპს მიეკუთვნება მოცემული მონაცემი
+
+
+
+a = 42                 
+b = "Hello, Python!"   
+c = 3.14              
+d = [1, 2, 3, 4]       
+e = True               
+
+
+print( type(a))  
+print( type(b))  
+print( type(c))  
+print( type(d))  
+print( type(e)) 
+
+
+
+
+x = 15
+y = 4
+
+mimateba = x + y 
+gamokleba = x - y    
+gamravleba = x * y 
+gayofa1 = x / y    
+gayofa2 = x // y 
+
+
+
+
+print( mimateba)
+print(gamokleba)
+print(gamravleba)
+print( gayofa1)
+print(gayofa2)
+
+
+
+
