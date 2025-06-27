df79b86c72721770c59d7c3d45e4a95b56248bb7
Giorgi965 <giogerliani229@gmail.com>
2025-06-27 18:56:10 +0400

    second commit

---
+++ goa/day28/classwork/cwlvl28.py
@@ -0,0 +1,17 @@
+foods=["ხაჭაპური","ხინკალი","კუბდარი","ბორში","ქათამი"]
+
+city=['london','madrid','barcelona',1,2,3]
+
+
+
+
+
+sia=["tbilisi",'qutaisi','batumi','soxumi','Wiatura',12,12,13,11,7,True,False,True,False,True]
+
+img=int(input("enrter num"))
+non=int(input("enter num"))
+num3=int(input("enter num"))
+
+sia[4]=img
+sia[7]=non
+sia[11]=num3
