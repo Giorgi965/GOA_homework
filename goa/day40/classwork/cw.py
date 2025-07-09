919ce1d2d0167ab9c5b8a9535f13fd4b98019559
Giorgi965 <giogerliani229@gmail.com>
2025-07-09 20:55:47 +0400

    Delete goa directory

--- goa/day40/classwork/cw.py
+++
@@ -1,6 +0,0 @@
-def multiply(a, b):
-    return a * b
-
-
-def even_or_odd(number):
-    return "Even" if number % 2 == 0 else "Odd"
--- goa/day42/classwork/cwlvl42.py
+++
@@ -1,6 +0,0 @@
-def monkey_count(n):
-    return list(range(1, n + 1))
-
-
-def array_plus_array(arr1,arr2):
-    return sum(arr1) + sum(arr2)
