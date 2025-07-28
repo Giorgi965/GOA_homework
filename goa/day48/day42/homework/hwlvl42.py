494f6c8dc3ed60e8e4279f49808741af24cad550
Giorgi965 <giogerliani229@gmail.com>
2025-07-28 14:18:58 +0400

    Add files via upload

---
+++ goa/day48/day42/homework/hwlvl42.py
@@ -0,0 +1,17 @@
+def boolean_to_string(b):
+    return str(b)
+
+
+def invert(lst):
+    return [-num for num in lst]
+
+
+def between(a,b):
+    return list(range(a, b + 1))
+
+
+def monkey_count(n):
+    return list(range(1, n + 1))
+
+def array_plus_array(arr1,arr2):
+    return sum(arr1) + sum(arr2)
