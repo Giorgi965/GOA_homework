e97f03e9fba7c7d35b484d1f05e881ed8a62ea96
Giorgi965 <giogerliani229@gmail.com>
2025-07-28 14:14:01 +0400

    second commit

---
+++ goa/day03/classwork/cwlvl03.py
@@ -0,0 +1,3 @@
+str='goa'
+int=62
+float=2.3
---
+++ goa/day40/homework/codewarslvl40.py
@@ -0,0 +1,20 @@
+def make_upper_case(s):
+    return s.upper()
+
+
+def simple_multiplication(number) :
+    if number % 2 == 0:
+        return number * 8
+    else:
+        return number * 9
+    
+
+def string_to_number(s):
+    return int (s)
+
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
